06defbac8bfaa626c4f7ef9bca50d7455ca4673c
SajesanGhimire <90140989+SajesanGhimire@users.noreply.github.com>
2022-06-23 12:44:02 +0545

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ var canvas = document.getElementById('image'),
 imageObj.onload = function() {
 	context.drawImage(imageObj, 69, 50);
 };
-imageObj.src = 'Disclaimer_Intro_Check_Description_For_The_information(720p).mp4';
+imageObj.src = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiJ58kzNphir9_Q_3AwYRCnJ2l7-Zpqg5lQftc8GYKm-zUpcfhVmGac5EaesGxaUPxp-jypoWOSP7W4TZiDwrTSSp-TmRUbkGOnl5WvvvtZ46Fu7Ohz1k6WF8-98AR7M27SIGY42NLhEF9UL2gilAo4pj_5eEqyxH7hcYZbBQKcx7MAoPH71ItrjGFC-A/s1280/fb.jpg';
 canvas.oncontextmenu = function() {return false};
